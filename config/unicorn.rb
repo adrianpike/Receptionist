94bb15b7fce117e4dacc7a60a8c804892f6a3e09
adrianpike <adrian.pike@gmail.com>
2010-10-27 12:30:30 -0700

    unicorns!

---
+++ config/unicorn.rb
@@ -0,0 +1,63 @@
+# Based on the github unicorn config 
+# <3 github
+
+# unicorn_rails -c /u/sharezen/current/config/unicorn.rb -E production -D
+
+rails_env = ENV['RAILS_ENV'] || 'production'
+
+worker_processes (rails_env == 'production' ? 4 : 4)
+
+# Load rails into the master before forking workers
+# for super-fast worker spawn times
+preload_app true
+
+# Restart any workers that haven't responded in 30 seconds
+timeout 30
+
+# Listen on a Unix data socket
+listen '/tmp/.sock'
+
+pid '/var/run/unicorn.pid'
+
+# ##
+# # REE
+# 
+# # http://www.rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
+# if GC.respond_to?(:copy_on_write_friendly=)
+#   GC.copy_on_write_friendly = true
+# end
+# 
+after_fork do |server, worker|
+  ##
+  # Unicorn master loads the app then forks off workers - because of the way
+  # Unix forking works, we need to make sure we aren't using any of the parent's
+  # sockets, e.g. db connection
+
+  ActiveRecord::Base.establish_connection
+  # Redis and Memcached would go here but their connections are established
+  # on demand, so the master never opens a socket
+
+
+  ##
+  # Unicorn master is started as root, which is fine, but let's
+  # drop the workers to git:git
+
+  begin
+    uid, gid = Process.euid, Process.egid
+    user, group = 'apps', 'apps'
+    target_uid = Etc.getpwnam(user).uid
+    target_gid = Etc.getgrnam(group).gid
+    worker.tmp.chown(target_uid, target_gid)
+    if uid != target_uid || gid != target_gid
+      Process.initgroups(user, target_gid)
+      Process::GID.change_privilege(target_gid)
+      Process::UID.change_privilege(target_uid)
+    end
+  rescue => e
+    if RAILS_ENV == 'development'
+      STDERR.puts "couldn't change user, oh well"
+    else
+      raise e
+    end
+  end
+end
